d73245cac3d5b55c7c8719e1428466fadf4bd854
Leandro Garcia <124641959+leandrogrc@users.noreply.github.com>
2025-03-14 12:06:53 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,32 @@
 function userScroll() {
-  const navbar = document.querySelector('.navbar');
+  const navbar = document.querySelector(".navbar");
 
-  window.addEventListener('scroll', () => {
+  window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
-      navbar.classList.add('bg-dark');
-      navbar.classList.add('navbar-sticky');
+      navbar.classList.add("bg-dark");
+      navbar.classList.add("navbar-sticky");
     } else {
-      navbar.classList.remove('bg-dark');
-      navbar.classList.remove('navbar-sticky');
+      navbar.classList.remove("bg-dark");
+      navbar.classList.remove("navbar-sticky");
     }
   });
 }
 
-document.addEventListener('DOMContentLoaded', userScroll);
+function setYear() {
+  const currentYear = new Date().getFullYear();
+  console.log(currentYear);
+  const elements = document.getElementsByClassName(
+    "text-end d-none d-md-block"
+  );
+  if (elements.length > 0) {
+    elements[0].innerHTML = `
+      <p class="text-end d-none d-md-block">
+        Copyright &copy; Blog Mastery ${currentYear}
+      </p>`;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  userScroll();
+  setYear();
+});
